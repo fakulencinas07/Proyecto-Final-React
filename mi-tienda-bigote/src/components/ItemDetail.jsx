99d77eef82eb0fcc5bb9b58146072b62f33a92ad
fakulencinas07 <fakulencinas@gmail.com>
2023-07-02 19:33:10 -0300

    first

---
+++ mi-tienda-bigote/src/components/ItemDetail.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import ItemCount from "./ItemCount"
+
+
+const ItemDetail = ({ item }) => {
+
+    const [cantidad, setCantidad] = useState(1);
+
+    const handleRestar = () => {
+        cantidad > 1 && setCantidad(cantidad -1)
+        }
+    
+    const handleSumar = () => {
+        cantidad < item.stock && setCantidad(cantidad +1)
+    }
+
+    const handleAgregar = () => {
+        console.log({...item, cantidad});
+    }
+
+
+    return (
+        <div className="container">
+            <div className="producto-detalle">
+                <img src={item.imagen} alt={item.titulo} />
+                <div>
+                    <h3 className="titulo">{item.titulo}</h3>
+                    <p className="descripcion"></p>
+                    <p className="categoria">Categoria: {item.categoria}</p>
+                    <p className="precio">${item.precio}</p>
+                    <ItemCount cantidad={cantidad} handleSumar={handleSumar} handleRestar={handleRestar} handleAgregar={handleAgregar} />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ItemDetail
